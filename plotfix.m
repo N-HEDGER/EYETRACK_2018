4682b968132931992f1d481e4ceec82e91ea1e5d
NICKHEDGE <nhedger1@gmail.com>
2018-01-11 16:03:22 +0000

    End of trial plotter

--- plotfix.m
+++ plotfix.m
@@ -1,10 +1,19 @@
-struct=load('gtu.mat');
-data=struct.config.gaze.collected_gaze_data(3).gaze
+struct=load('billboy_trial1_gaze.mat');
+data=struct.collected_gaze_data
 Xval=zeros(1,299);
 Yval=zeros(1,299);
 for i=1:299
 instance=data(i);
-Xval(i)=instance.LeftEye.GazePoint.OnDisplayArea(1);
-Yval(i)=instance.LeftEye.GazePoint.OnDisplayArea(2);
+Xval(i)=instance.LeftEye.GazePoint.OnDisplayArea(1)*config.scr.;
+Yval(i)=instance.LeftEye.GazePoint.OnDisplayArea(2)*1080;
 end
 plot(Xval,Yval)
+axis([-10 1930 -10 1090],'fill')
+rectangle('Position',[0 0 1920 1080])
+rectangle('Position',[hj.stimrectl(1) hj.stimrectl(2) hj.stimrectl(3)-hj.stimrectl(1) hj.stimrectl(4)-hj.stimrectl(2)])
+rectangle('Position',[hj.stimrectr(1) hj.stimrectr(2) hj.stimrectr(3)-hj.stimrectr(1) hj.stimrectr(4)-hj.stimrectr(2)])
+
+
+
+
+
